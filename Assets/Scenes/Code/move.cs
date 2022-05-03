c33e5412b5a9d190cd277def9ff6732515514cd7
Tsunami909 <salvatore.disanza909@gmail.com>
2022-05-03 16:11:22 +0200

    update

--- Assets/Scenes/Code/move.cs
+++ Assets/Scenes/Code/move.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public int moveSpeed = 100;
-    public float forceAmount = 1.0f;
+    public float forceAmount = 10.0f;
+    public GameObject Parete1;
+    public GameObject Parete2;
     public GameObject Parete4;
+    public GameObject PareteCentrale;
+    public GameObject PareteLungaInt1;
+    public GameObject PareteLungaInt2;
+    public GameObject ParetePiccolaInt1;
+    public GameObject ParetePiccolaInt2;
     public Rigidbody rb;
 
     void Start()
@@ -55,12 +62,57 @@ public class move : MonoBehaviour
             rb.AddForce(Vector3.back * forceAmount, ForceMode.Impulse);
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == Parete4)
+        {
+            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+        }
+
+        if(collision.gameObject == PareteCentrale)
+        {
+            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+        }
+
+        if(collision.gameObject == PareteLungaInt1)
+        {
+            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+        }
+
+        if(collision.gameObject == PareteLungaInt2)
+        {
+            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+        }
+
+        if(collision.gameObject == ParetePiccolaInt1)
+        {
+            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+        }
+        if(collision.gameObject == ParetePiccolaInt2)
+        {
+            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+        }
+
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject == Parete1)
+        {
+            Destroy(Parete1);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject == Parete2)
+        {
+            Parete2.GetComponent<Renderer>().material.color = Color.Lerp(Parete2.GetComponent<Renderer>().material.color, new Color(Random.value, Random.value, Random.value, Random.value), Random.value);
+        }
+    }
+
+    
 }
 
-void private OnCollisionEnter(Collision collision)
-{
-    if(collision.gameObject == Parete4)
-    {
-        GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
-    }
-}
